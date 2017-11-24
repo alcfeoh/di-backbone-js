4a36800a13f5a3206fd59e1eb44fc473b9ac39bc
Alain Chautard <achautard@gmail.com>
2017-11-24 12:34:29 -0800

    Lab #7 ready

---
+++ solutions/7-fetch-plates/app.js
@@ -0,0 +1,45 @@
+$(function() {
+
+    var LicensePlate = Backbone.Model.extend({});
+
+
+    var LicensePlateView = Backbone.View.extend({
+        tagName:  "div",
+        attributes: {class: 'col-md-4', style: 'margin-top: 40px'},
+        initialize: function () {
+            this.render();
+        },
+        render: function () {
+            var source = $('#plate-template').html();
+            var template = Handlebars.compile(source);
+            var html = template(this.model.toJSON());
+            this.$el.html(html);
+            return this;
+        }
+    });
+
+    var LicensePlateList = Backbone.Collection.extend({
+        model: LicensePlate,
+        url: 'http://localhost:8080/plates.json'
+    });
+
+    var plateList = new LicensePlateList();
+
+    var AppView = Backbone.View.extend({
+
+        el: "#container",
+
+        initialize: function () {
+            this.listenTo(plateList, "add", this.addPlate);
+            plateList.fetch();
+        },
+        addPlate: function(plate) {
+            let model = new LicensePlateView({model: plate});
+            this.$el.append(model.render().el);
+        }
+
+    });
+
+    var app = new AppView();
+
+});
