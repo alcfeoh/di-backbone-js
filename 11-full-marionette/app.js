cbcfac77c213aa2127e606dd8800ebc14d622aed
Alain Chautard <alain@interstate21.com>
2017-11-30 19:56:31 -0800

    Lab 11 improved

--- 11-full-marionette/app.js
+++ 11-full-marionette/app.js
@@ -2,10 +2,6 @@ $(function() {
 
     var LicensePlate = Backbone.Model.extend({});
 
-    var CartItem = Backbone.Model.extend({
-        url: '/cart'
-    });
-
     var LicensePlateView = Backbone.View.extend({
         tagName:  "div",
         attributes: {class: 'col-md-4', style: 'margin-top: 40px'},
@@ -36,13 +32,6 @@ $(function() {
 
     var plateList = new LicensePlateList();
 
-    var CartPlateList = Backbone.Collection.extend({
-        model: LicensePlate,
-        url: '/cartContents'
-    });
-
-    var cartPlateList = new CartPlateList();
-
     var StoreView = Backbone.View.extend({
 
         el: "#container",
@@ -57,49 +46,8 @@ $(function() {
         }
     });
 
-    var CartView = Backbone.View.extend({
-
-        el: "#container",
-
-        initialize: function () {
-            this.listenTo(cartPlateList, "add", this.addPlate);
-            cartPlateList.fetch();
-        },
-        addPlate: function(plate) {
-            let model = new LicensePlateView({model: plate});
-            this.$el.append(model.render().el);
-        }
-
-    });
 
     var view = new StoreView();
 
-    var Router = Backbone.Router.extend({
-
-        routes: {
-            "store":    "store",
-            "cart":     "cart"
-        },
-
-        store: function() {
-            view.stopListening();
-            $('#container').html('');
-            $('#storeHeader').show();
-            $('#cartHeader').hide();
-            view = new StoreView();
-        },
-
-        cart: function(query, page) {
-            view.stopListening();
-            $('#container').html('');
-            $('#storeHeader').hide();
-            $('#cartHeader').show();
-            view = new CartView();
-        }
-
-    });
-
-    var router = new Router();
-    Backbone.history.start();
 
 });
